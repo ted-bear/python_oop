c0f923f736c6475226f36f589805cbd938a1f102
e.d.toporkov <e.d.toporkov@tbank.ru>
2025-11-15 23:51:31 +0300

    add example and homework for first lesson

---
+++ first_hw/second.py
@@ -0,0 +1,45 @@
+# Смоделируем приложение для публикации своих текстов с подписочной системой:
+# Ключевые классы:
+# 1. Пользователь ()
+# 2. Статья
+# 3. Подписка
+
+class Subscription:
+    id: int
+    name: str # название подписки
+    description: str # описание подписки
+    author_id: int # автор подписки
+    amount: float # стоимость подписки
+    type: str # тип подписки (ежегодная, ежемесечная, единоразовая и т.д.)
+    is_archived: bool # заархивирована ли подписка (недоступна для покупки)
+
+
+class User:
+    id: int = 1
+    nickname: str = 'username' # Никнейм пользователя
+    subscriptions: list[int] = [] # Подписки пользователя
+    description: str = 'chill' # Описание аккаунта
+
+    def print_user(self):
+        print(f'### User: {self.id}')
+        print(f'### Nickname: {self.nickname}')
+        print(f'### Description: {self.description}')
+        print(f'### Subscriptions: {self.subscriptions}')
+
+class Article:
+    id: int
+    author_id: int # ИД автора статьи
+    name: str # название статьи
+    create_time: int # время создания статьи
+    subscription_type: list[int] # Список ИД подписок, которым доступен этот материал
+    text: str # Содержание статьи
+    links: str # Ссылки на доп. материалы
+
+user_1 = User()
+user_1.print_user()
+user_2 = user_1
+user_2.print_user()
+
+user_2.nickname = 'changed userName'
+user_1.print_user()
+user_2.print_user()
---
+++ oop/cat.py
@@ -0,0 +1,9 @@
+class Cat:
+    name = 'Барсик'
+    age = 0
+    weight = 0.1
+    speed = 0
+    fq = 100
+
+my_cat = Cat()
+print(my_cat.name)
